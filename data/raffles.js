d651201509bf85cd22b25882c7d50cde660528cc
David <gelpiorama@gmail.com>
2019-03-11 12:33:15 +0100

    modulo raffles y readme reutilizado

---
+++ data/raffles.js
@@ -0,0 +1,69 @@
+
+var raffles = {
+
+    "Antonia Milano": {
+        "country": "Italy",
+        "purchase": "Online Raffle",
+        "collection": "Postage Available",
+        "Sizes": "4 to 12 US",
+        "Opens": "live",
+        "Closes": "06/03 @ 11AM CET"
+    },
+
+    "END": {
+        "country": "UK",
+        "purchase": "Online Raffle",
+        "collection": "Postage Available",
+        "Sizes": "5 to 12 UK",
+        "Opens": "live",
+        "Closes": "07/03 @ 12AM GMT"
+    },
+
+    "Foot Patrol": {
+        "country": "France",
+        "purchase": "Online Raffle",
+        "collection": "Collection Only",
+        "Sizes": "36.5 to 47.5 EU",
+        "Opens": "live",
+        "Closes": "04/02 @ 10AM CET"
+    },
+
+    "Holypop": {
+        "country": "Italy",
+        "purchase": "Online FCFS",
+        "collection": "Postage Available",
+        "Sizes": "TBC",
+        "Opens": "announced",
+        "Closes": "07/02 @ 12AM CET"
+    },
+
+    "Offspring": {
+        "country": "UK",
+        "purchase": "Online Raffle",
+        "collection": "Collection Only",
+        "Sizes": "3.5 to 7 UK",
+        "Opens": "live",
+        "Closes": "closed"
+    },
+
+    "SNS": {
+        "country": "Swe, UK, Ger, Fr",
+        "purchase": "Online Raffle",
+        "collection": "Post and Collect",
+        "Sizes": "4 to 13 US",
+        "Opens": "live",
+        "Closes": "06/03 @ 11AM CET"
+    },
+
+    "Solebox": {
+        "country": "Germany",
+        "purchase": "In-Store/Online",
+        "collection": "Post and Collect",
+        "Sizes": "41 to 46 EU",
+        "Opens": "announced",
+        "Closes": "When sold out"
+    },
+
+};
+
+exports.raffles = raffles;
